6f0328adaef12c79c721535f74e98e1639d6f2e5
yashhtech <yash15778753x@gmail.com>
2026-01-24 22:16:43 +0530

    updating topbar

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -11,9 +11,7 @@ const TopBar = ({ show }) => {
   transition={{ type: "spring", stiffness: 200, damping: 20 }}
   className="
     fixed top-0 left-0 w-full px-10 py-2 text-sm
-    bg-linear-to-r from-white/70 via-emerald-50/60 to-teal-100/60
-    backdrop-blur-xl
-    border-b border-white/40
+    bg-blaack
     shadow-lg z-50
   "
 >
@@ -25,7 +23,7 @@ const TopBar = ({ show }) => {
       key={i}
       whileHover={{
         scale: 1.1,
-        color: "#059669",
+        color: "blue",
         textShadow: "0 0 10px rgba(16,185,129,0.6)",
       }}
       transition={{ type: "spring", stiffness: 300 }}
@@ -43,7 +41,7 @@ const TopBar = ({ show }) => {
             onClick={() => navigate("/Signin")}
             whileHover={{ scale: 1.05, backgroundColor: "#10B981", color: "#fff" }}
             transition={{ type: "spring", stiffness: 300 }}
-            className="px-4 py-1 rounded-full border border-white text-white font-semibold shadow-sm"
+            className="px-4 py-1 bg-black rounded-full border border-white text-white font-semibold shadow-sm"
           >
             Sign In
           </motion.button>
@@ -52,7 +50,7 @@ const TopBar = ({ show }) => {
             onClick={() => navigate("/Signup")}
             whileHover={{ scale: 1.05, backgroundColor: "#06B6D4", color: "#fff" }}
             transition={{ type: "spring", stiffness: 300 }}
-            className="px-4 py-1 rounded-full bg-white text-teal-600 font-semibold shadow-sm"
+            className="px-4 py-1 rounded-full bg-black text-white font-semibold shadow-sm"
           >
             Sign Up
           </motion.button>
